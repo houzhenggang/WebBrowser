56022fb6919b6563545fca16c5930d695abccb76
apple <1754410821@qq.com>
2016-02-15 20:15:17 +0800

    update
    提交更新

--- WebBrowser/Classes/Controllers/WebBrowserViewController.swift
+++ WebBrowser/Classes/Controllers/WebBrowserViewController.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2016年 X.R. All rights reserved.
 //
 
+/**
+ *  WebView实现简易浏览器
+ */
+
 import UIKit
 
 class WebBrowserViewController: UIViewController, UIWebViewDelegate, UIAlertViewDelegate {
@@ -13,15 +17,21 @@ class WebBrowserViewController: UIViewController, UIWebViewDelegate, UIAlertView
     @IBOutlet weak var btnWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var activityView: UIActivityIndicatorView!
     @IBOutlet weak var myWebView: UIWebView!
+    @IBOutlet weak var searchTextField: UITextField!
+    var webUrl: String?
     
     func initialWebView() -> Void {
         // 设置webView
         myWebView.backgroundColor = UIColor.clearColor()
         myWebView.scalesPageToFit = true
         myWebView.delegate = self
-        
+        self.webUrl = "http://www.hao123.com" // 默认首页网址
+    }
+    
+    // 加载网页
+    func loadWebSiteFromURL() -> Void {
         // 创建请求
-        let requestUrl = NSURL(string: "http://www.hao123.com")
+        let requestUrl = NSURL(string: self.webUrl!)
         let webRequest = NSURLRequest(URL: requestUrl!, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: 60)
         myWebView.loadRequest(webRequest)
     }
@@ -81,6 +91,25 @@ class WebBrowserViewController: UIViewController, UIWebViewDelegate, UIAlertView
         self.initialActivityView()
         self.initialWebView()
         self.btnWidthConstraint.constant = UIScreen.mainScreen().bounds.size.width / 4.0
+        let attributesStr = NSAttributedString(string: "请输入您要访问的网址", attributes: [NSForegroundColorAttributeName : UIColor.whiteColor()])
+        self.searchTextField.attributedPlaceholder = attributesStr
+        self.searchTextField.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.6)
+        self.searchTextField.textAlignment = NSTextAlignment.Center
+        self.searchTextField.textColor = UIColor.whiteColor()
+        self.loadWebSiteFromURL()
+    }
+    
+    @IBAction func gotoWebSiteAction(sender: AnyObject) {
+        self.webUrl = self.searchTextField.text
+        if let has = self.webUrl?.hasPrefix("http://") {
+            if has {
+                self.webUrl = self.searchTextField.text
+            }else {
+                self.webUrl = "http://" + self.webUrl!
+            }
+        }
+        self.loadWebSiteFromURL()
+        self.view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
