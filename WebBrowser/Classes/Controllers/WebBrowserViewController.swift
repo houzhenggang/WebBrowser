2cacde1b651c204ed6c8b942fd5d50ab2e1477d9
apple <1754410821@qq.com>
2016-02-16 14:13:54 +0800

    commit changes
    提交修改

--- WebBrowser/Classes/Controllers/WebBrowserViewController.swift
+++ WebBrowser/Classes/Controllers/WebBrowserViewController.swift
@@ -12,55 +12,30 @@
 
 import UIKit
 
-class WebBrowserViewController: UIViewController, UIWebViewDelegate, UIAlertViewDelegate {
+class WebBrowserViewController: UIViewController, UIWebViewDelegate, UIAlertViewDelegate, NJKWebViewProgressDelegate {
     
     @IBOutlet weak var btnWidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var activityView: UIActivityIndicatorView!
     @IBOutlet weak var myWebView: UIWebView!
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var progressView: NJKWebViewProgressView!
+    
     var webUrl: String?
+    var progressProxy: NJKWebViewProgress?
     
     func initialWebView() -> Void {
         // 设置webView
         myWebView.backgroundColor = UIColor.clearColor()
         myWebView.scalesPageToFit = true
         myWebView.delegate = self
-        self.webUrl = "http://www.hao123.com" // 默认首页网址
+        self.webUrl = "http://m.hao123.com" // 默认首页网址
     }
     
     // 加载网页
     func loadWebSiteFromURL() -> Void {
         // 创建请求
         let requestUrl = NSURL(string: self.webUrl!)
-        let webRequest = NSURLRequest(URL: requestUrl!, cachePolicy: NSURLRequestCachePolicy.ReloadRevalidatingCacheData, timeoutInterval: 60)
+        let webRequest = NSURLRequest(URL: requestUrl!)
         myWebView.loadRequest(webRequest)
-    }
-    
-    // 开始加载
-    func webStartLoading() -> Void {
-        if !self.activityView.isAnimating() {
-            self.view.userInteractionEnabled = false
-            self.myWebView.hidden = true
-            self.activityView.startAnimating()
-        }
-    }
-    
-    // 停止加载
-    func webStopLoading() -> Void {
-        if self.activityView.isAnimating() {
-            self.view.userInteractionEnabled = true
-            self.activityView.stopAnimating()
-            self.myWebView.hidden = false
-        }
-    }
-    
-    func initialActivityView() -> Void {
-        // 初始化加载View
-        self.activityView.color = UIColor.grayColor()
-        self.activityView.hidesWhenStopped = true
-        
-        // 立刻开始转
-        self.webStartLoading()
     }
     
     @IBAction func goBackAction(sender: AnyObject) {
@@ -88,28 +63,45 @@ class WebBrowserViewController: UIViewController, UIWebViewDelegate, UIAlertView
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.initialActivityView()
         self.initialWebView()
         self.btnWidthConstraint.constant = UIScreen.mainScreen().bounds.size.width / 4.0
         let attributesStr = NSAttributedString(string: "请输入您要访问的网址", attributes: [NSForegroundColorAttributeName : UIColor.whiteColor()])
         self.searchTextField.attributedPlaceholder = attributesStr
-        self.searchTextField.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.6)
+        self.searchTextField.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.7)
         self.searchTextField.textAlignment = NSTextAlignment.Center
         self.searchTextField.textColor = UIColor.whiteColor()
         self.loadWebSiteFromURL()
+        // 初始化加载进度条
+        self.progressProxy = NJKWebViewProgress()
+        self.myWebView.delegate = self.progressProxy
+        self.progressProxy?.webViewProxyDelegate = self
+        self.progressProxy?.progressDelegate = self
     }
     
     @IBAction func gotoWebSiteAction(sender: AnyObject) {
         self.webUrl = self.searchTextField.text
-        if let has = self.webUrl?.hasPrefix("http://") {
-            if has {
-                self.webUrl = self.searchTextField.text
-            }else {
-                self.webUrl = "http://" + self.webUrl!
+        
+        // 判断webURL是否合法
+        if let url = self.webUrl where !url.isEmpty {
+            if let has = self.webUrl?.hasPrefix("http://") {
+                if has {
+                    self.webUrl = self.searchTextField.text
+                }else {
+                    self.webUrl = "http://" + self.webUrl!
+                }
+            }else if let hasHttps = self.webUrl?.hasPrefix("https://") {
+                if hasHttps {
+                    self.webUrl = self.searchTextField.text
+                }
             }
+            // 显示URL
+            if self.webUrl == "http://" || self.webUrl == "https://" {
+                self.webUrl = ""
+            }
+            self.searchTextField.text = self.webUrl
+            self.loadWebSiteFromURL()
+            self.searchTextField.endEditing(true)
         }
-        self.loadWebSiteFromURL()
-        self.view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
@@ -119,26 +111,25 @@ class WebBrowserViewController: UIViewController, UIWebViewDelegate, UIAlertView
     
     // MARK: UIWebViewDelegate
     func webViewDidStartLoad(webView: UIWebView) {
-        print("\(self.myWebView.request?.URL?.absoluteString)")
-        self.webStartLoading()
+        self.webUrl = webView.request?.URL?.absoluteString
+        // 显示URL
+        if self.webUrl == "http://" || self.webUrl == "https://" {
+            self.webUrl = ""
+        }
+        self.searchTextField.text = self.webUrl
+        self.searchTextField.endEditing(true)
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
-        self.webStopLoading()
     }
     
     func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
-        self.webStopLoading()
-        // 弹出alert
-        let alert = UIAlertView(title: "加载失败", message: "", delegate: self, cancelButtonTitle: "关闭", otherButtonTitles: "重新加载")
-        alert.show()
+        print("加载遇到错误: \(error?.localizedFailureReason)")
     }
     
-    // MARK: UIAlertViewDelegate
-    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
-        if buttonIndex != alertView.cancelButtonIndex {
-            self.myWebView.reload()
-        }
+    // MARK: NJKWebViewProgressDelegate
+    func webViewProgress(webViewProgress: NJKWebViewProgress!, updateProgress progress: Float) {
+        self.progressView.setProgress(progress, animated: true)
     }
 }
 
---
+++ WebBrowser/OC_BridgingHeader.h
@@ -0,0 +1,18 @@
+//
+//  OC_BridgingHeader.h
+//  WebBrowser
+//
+//  Created by xuran on 16/2/16.
+//  Copyright © 2016年 X.R. All rights reserved.
+//
+
+// OC - Swift bridge Header
+// Swift - OC bridge Header  WebBrowser-Swift.h
+
+#ifndef OC_BridgingHeader_h
+#define OC_BridgingHeader_h
+
+#import "NJKWebViewProgressView.h"
+#import "NJKWebViewProgress.h"
+
+#endif /* OC_BridgingHeader_h */
